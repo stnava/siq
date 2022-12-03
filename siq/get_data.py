fe5c2eff5a385564309ffea4f810fb6be1242447
stnava <stnava@gmail.com>
2022-12-03 16:41:28 -0500

    WIP: ...

--- siq/get_data.py
+++ siq/get_data.py
@@ -1215,17 +1215,17 @@ def compare_models( model_filenames, img, verbose=False ):
             dimgup=inference( dimg, srmdl, segmentation = dimgotsu, verbose=False )['super_resolution']
         else:
             dimgup=inference( dimg, srmdl, verbose=False  )
-        dimglin = ants.resample_image_to_target( dimg, dimgup )
-        if not ants.image_physical_space_consistency(img,dimgup) :
-            imgblock = ants.resample_image_to_target( img, dimgup )
+        dimglin = ants.resample_image_to_target( dimg, dimgup, interp_type='linear' )
+        if not ants.image_physical_space_consistency(img,dimgup ) :
+            imgblock = ants.resample_image_to_target( img, dimgup, interp_type='linear'  )
         else:
             imgblock = img
         padder = []
         for jj in range(img.dimension):
             padder.append( -2 )
-        dimgup = ants.pad_image( dimgup, padder )
-        dimglin = ants.pad_image( dimglin, padder )
-        imgblock = ants.pad_image( imgblock, padder )
+#        dimgup = ants.pad_image( dimgup, padder )
+ #       dimglin = ants.pad_image( dimglin, padder )
+  #      imgblock = ants.pad_image( imgblock, padder )
         temp = os.path.basename( model_filenames[k] )
         temp = re.sub( "siq_default_sisr_", "", temp )
         temp = re.sub( "_best_mdl.h5", "", temp )
