f1b008e43443f8bd945fe2aa0ac74a0d9eb18f5b
stnava <stnava@gmail.com>
2022-12-24 11:41:16 -0500

    BUG: ... in seg inference ...

--- siq/get_data.py
+++ siq/get_data.py
@@ -1460,8 +1460,8 @@ def compare_models( model_filenames, img, n_classes=3, identifier=None, verbose=
             seghigh = ants.threshold_image( img,"Otsu",n_classes)
             seglow = ants.resample_image( seghigh, tarshape, use_voxels=False, interp_type=1 )
             dimgup=inference( dimg, srmdl, segmentation = seglow, verbose=verbose )
-            dimgup = dimgup['super_resolution']
             dimgupseg = dimgup['super_resolution_segmentation']
+            dimgup = dimgup['super_resolution']
             segimgnn = ants.resample_image_to_target( seglow, dimgupseg, interp_type='nearestNeighbor' )
             dicenn = ants.label_overlap_measures(seghigh, segimgnn)['MeanOverlap'][0]
             dicesr = ants.label_overlap_measures(seghigh, dimgupseg)['MeanOverlap'][0]
